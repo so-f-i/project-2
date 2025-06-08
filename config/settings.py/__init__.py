42844bbcddff6d33c3b65c0c5202d434e2a93aa6
so-f-i <sonya.mavlenko.02@gmail.com>
2025-06-08 17:32:03 +0300

    Delete config directory

--- config/settings.py/__init__.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "ini.env")
-load_dotenv(dotenv_path)
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-ADMINS = list(map(int, os.getenv("ADMINS", "").split(",")))
